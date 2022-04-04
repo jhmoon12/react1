1c0599ebb319f78bb8a233244d0edcdf12e0b8b4
jhmoon12 <mjh9461@gmail.com>
2022-04-05 01:37:39 +0900

    updated customHook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React,{ useRef, useState, useMemo, useCallback } from 'react';
+import React,{ useRef, useState, useMemo, useCallback, useReducer } from 'react';
 import './App.css';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
 import Counter from './Counter';
+import useInputs from './useInputs';
 
 
 function countActiveUsers(users) {
@@ -10,95 +11,116 @@ function countActiveUsers(users) {
   return users.filter(users => users.active).length;
 }
 
+const initialState = {
+ 
+  users: [
+    {
+      id: 1,
+      username: 'moon',
+      email: 'moon@gmail.com',
+      active: true,
+  },
+  {
+      id: 2,
+      username: 'Park',
+      email: 'Park@gmail.com',
+      active: false,
+  },
+  {
+      id: 3,
+      username: 'Kim',
+      email: 'Kim@gmail.com',
+      active: false,
+  }
+  ]
+};
+function reducer(state, action){
+  switch (action.type){
+    
+    case 'CREATE_USER': {
+      return {
+        inputs: initialState.inputs, //초기값
+        users: state.users.concat(action.user) //배열 추가
+      }
+    };// useState를 썼을 때는 기존 작업 두가지를 따로 했는데, 이곳에서는 한 번에 할 수 있다.
+    case 'TOGGLE_USER': {
+      return {
+        ...state,
+        users: state.users.map(user => 
+          user.id === action.id
+          ? {...user, active: !user.active }
+          : user
+        )
+      }
+    };
+    case 'REMOVE_USER': {
+      return {
+        ...state,
+        users: state.users.filter( user => user.id !== action.id)
+      }
+    };
+    default : 
+      throw new Error('unHandled Error')
+  };
+  
+};
+
 function App() {
 
-  const [inputs, setInputs] = useState({
-   username : '',
-   email : ''
-  });
-  const { username, email } = inputs; //구조분해할당
+  const [ state, dispatch ] = useReducer(reducer, initialState);
+  const [ form, onChange, reset ] = useInputs({
+    username : '',
+    email: '',
+  })
+  const nextId = useRef(4);
+  const { users } = state; //구조분해할당, 현재 상태가 state 이니 state 를 가져오는 것
 
-  const onChange = useCallback((e) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name] : value
-    });
-  },[inputs]);
-  //onChange 함수
-
-  const [users,setUsers] = useState([{
-    id: 1,
-    username: 'moon',
-    email: 'moon@gmail.com',
-    active: true,
-},
-{
-    id: 2,
-    username: 'Park',
-    email: 'Park@gmail.com',
-    active: false,
-},
-{
-    id: 3,
-    username: 'Kim',
-    email: 'Kim@gmail.com',
-    active: false,
-},
-]);
-
-const nextId = useRef(4);//컴포넌트가 리 렌더링 될 필요 없기 때문에 ref 로 관리해주는 것
-
-const onCreate = useCallback(() => {
-  const user = {
-    id: nextId.current,
-    username,
-    email,
-  };
-  setUsers(users => users.concat(user)); 
-  //1. 기존 배열을 복사하고 그 뒤에 user 객체 추가!, 2.함수형 업데이트(이 작업으로 deps 에 users 를 쓸 필요가 없다.)
-  setInputs({
-    username: '',
-    email: ''
-  });
-  //초기화
   
-  nextId.current += 1; //기존 값에 1을 더해주는 작업
-},[username, email])
-/* User 추가 함수 */
 
-const onRemove = useCallback( id => {
-  setUsers(users => users.filter( user => user.id !== id)); //user.id !== id 가 true 일때 나머지 배열만 새 배열로 만듦
-},[]); 
-/* 리스트 삭제 함수 */
+  const onCreate = useCallback(()=>{
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      }
+    });
+    nextId.current += 1;
+    reset();
+  }, [ username, email, reset ]);
 
-const onToggle = useCallback( id => {
-  setUsers(users => users.map(
-    user => user.id === id ? 
-    {...user, active: !user.active }
-    : user
-  ));
-}, []); //컴포넌트가 처음 만들어질때만 생성되고, 그 다음 재 사용된다.
-/* 토글 함수 */
+  const onToggle = useCallback((id) => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    })
+  }, []);
 
-const count = useMemo(()=>countActiveUsers(users), [users]);
-/* 활성화 리스트 체크 함수 */
+  const onRemove = useCallback((id)=>{
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    })
+  }, []);
+  
+  const count = useMemo(()=> countActiveUsers(users), [users])
+
 
   return (
     <div>
       <CreateUser 
       username={username}
-      email = {email}
+      email={email}
       onChange={onChange}
       onCreate={onCreate}
       />
       <UserList 
       users={users}
-      onRemove={onRemove}
       onToggle={onToggle}
+      onRemove={onRemove}
       />
       <div>활성 사용자 수 : {count}</div>
-      <Counter />
     </div>
   );
 }
