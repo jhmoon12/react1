f84be585c1fc3a635fc1f24a06dee2e2a97f0982
jhmoon12 <mjh9461@gmail.com>
2022-04-05 18:58:30 +0900

    useInput

--- src/useInputs.js
+++ src/useInputs.js
@@ -1,13 +1,37 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useReducer } from "react";
+
+function reducer (state, action) {
+    switch(action.type){
+        case "CHANGE" : {
+            return {
+                ...state, 
+                [action.name] : action.value
+            }
+        };
+        case "RESET" : {
+            return Object.keys(state).reduce((acc, current) => {
+                acc[current] = '';
+                return acc;
+              }, {});
+        };//이부분은 진짜 진짜 모르겠음 ㅜㅜ
+        default : 
+            return state;
+    };
+} ;
 
 function useInputs(initialForm) {
-    const [ form, setForm ] = useState(initialForm);
-    const onChange = useCallback(e => {
+   // const [ form, setForm ] = useState(initialForm);
+    const [ form, dispatch ] = useReducer( reducer, initialForm );
+    /* const onChange = useCallback(e => {
         const { name, value } = e.target;
         setForm(form => ({...form, [name]: value }));
+    }, []); */
+    const onChange = useCallback(e => {
+        const { name, value } = e.target;
+        dispatch({ type: "CHANGE", name, value });
     }, []);
-    const reset = useCallback(() => setForm(initialForm), [initialForm]);
-
+    //const reset = useCallback(() => setForm(initialForm), [initialForm])
+    const reset = useCallback(() => dispatch({ type: "RESET" }), [])
     return [form, onChange, reset];
 };
 
